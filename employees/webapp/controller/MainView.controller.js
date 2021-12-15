6d3e947035a56e06952eb7851b59f325b6532fa9
jcnuevo4 <jcnuevo4@gmail.com>
2021-12-15 00:22:56 +0000

    First Commit 04

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,20 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+        var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
 
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+            if (valueEmployee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
             }
-        });
+        };
+        return Main;
     });
